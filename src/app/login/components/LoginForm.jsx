ab1e4b444aa2859819332b5f9c0d2b171172a0cb
babladey275 <babladey275@gmail.com>
2025-03-25 23:42:55 +0600

    added login form with next auth functiionality

--- src/app/login/components/LoginForm.jsx
+++ src/app/login/components/LoginForm.jsx
@@ -1,29 +1,32 @@
 "use client";
-import { registerUser } from "@/app/actions/auth/registerUser";
-import Link from "next/link";
 import React from "react";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
 
-export default function RegisterForm() {
-  const handleSubmit = async (e) => {
+export default function LoginForm() {
+  // const router = useRouter();
+
+  const handleLogin = async (e) => {
     e.preventDefault();
     const form = e.target;
-    const name = form.name.value;
     const email = form.email.value;
     const password = form.password.value;
-    registerUser({ name, email, password });
+
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        callbackUrl: "/",
+        redirect: false,
+      });
+      // router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="fieldset w-full max-w-lg">
-      <label className="fieldset-label font-bold">Name</label>
-      <input
-        name="name"
-        type="text"
-        className="input w-full"
-        placeholder="Name"
-        required
-      />
-
+    <form onSubmit={handleLogin} className="w-full max-w-lg">
       <label className="fieldset-label font-bold mt-8">Email</label>
       <input
         name="email"
@@ -42,15 +45,15 @@ export default function RegisterForm() {
         required
       />
 
-      <button className="w-full h-12 my-8 bg-[#FF3811] text-white font-bold cursor-pointer">
-        Sign Up
+      <button className="w-full h-12 my-8 bg-[#FF3811] text-white cursor-pointer font-bold">
+        Sign In
       </button>
       <p className="text-center">Or Sign In with</p>
 
       <p className="text-center">
-        Don't Have an account?{" "}
-        <Link href="/login" className="text-[#FF3811] font-bold">
-          Login
+        Already have an account?{" "}
+        <Link href="/register" className="text-[#FF3811] font-bold">
+          Register
         </Link>
       </p>
     </form>
